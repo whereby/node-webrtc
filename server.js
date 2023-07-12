ef268cd869e62445f3220ebcd8a84af72e686094
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2023-07-12 16:43:13 -0400

    Update server so that it works

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const { RTCVideoSource, RTCAudioSource } = require('wrtc').nonstandard;
+const {RTCVideoSource, RTCAudioSource} = require('wrtc').nonstandard;
 const beamcoder = require('beamcoder');
-const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
+const {setIntervalAsync, clearIntervalAsync} = require('set-interval-async');
+const fs = require('fs');
 
 // Creates a MediaStream of a looping video/audio based on data stored in filename. Uses beamcoder to transcode the data from the file
 // and wrtc to publish those to a created stream.
@@ -16,37 +17,45 @@ function createArtificialMediaStream(filename) {
   // Create callbacks that will continuously push frames to those tracks
   // TODO(jack): add more configuration options besides just reading from file.
   beamcoder.demuxer(`file:${filename}`).then(demuxer => {
-    const videoDecoderCodec = { name: "vp9" };
-    const videoEncoderCodec = { name: "rawvideo", pix_fmt: "yuv420p" };
-    let videoDecoder = beamcoder.decoder(videoDecoderCodec);
+    let videoStreamIndex = -1;
+    let audioStreamIndex = -1;
+    let i = 0;
+    for (const stream of demuxer.streams) {
+      if (stream.codecpar.codec_type === 'video') {
+        videoStreamIndex = i;
+      } else if (stream.codecpar.codec_type === 'audio') {
+        audioStreamIndex = i;
+      }
+      ++i;
+    }
+    const videoEncoderCodec = {name: "rawvideo", pix_fmt: "yuv420p"};
+    let videoDecoder = beamcoder.decoder({demuxer, stream_index: videoStreamIndex});
     let videoEncoder = beamcoder.encoder(videoEncoderCodec);
-    const audioDecoderCodec = { name: "opus" };
-    const audioEncoderCodec = { name: "pcm_s16le", sample_fmt: "s16" };
-    let audioDecoder = beamcoder.decoder(audioDecoderCodec);
-    let audioEncoder = beamcoder.encoder(audioEncoderCodec);
-    let videoFrames = [];
-    let audioFrames = [];
+    // const audioEncoderCodec = {name: "pcm_s16le", sample_fmt: "s16"};
+    let audioDecoder = beamcoder.decoder({demuxer, stream_index: audioStreamIndex});
+    // let audioEncoder = beamcoder.encoder(audioEncoderCodec);
+    // TODO(jack): figure out why node-webrtc wants *exactly* this much data anyways
+    // Not a problem here since the packets happen to be exactly the right size, but still
+    const numberOfFrames = 480;
+    const intSamples = new Int16Array(numberOfFrames * 2);
     async function nextFrames() {
       let packet = await demuxer.read();
       if (packet == null) {
         // Stream ended, loop back to beginning
         console.log(`createArtificialMediaStream: restarting stream for ${filename}`);
+        await demuxer.seek({time: 0});
         await videoDecoder.flush();
-        videoDecoder = beamcoder.decoder(videoDecoderCodec);
+        videoDecoder = beamcoder.decoder({demuxer, stream_index: videoStreamIndex});
         await audioDecoder.flush();
-        audioDecoder = beamcoder.decoder(audioDecoderCodec);
-        await demuxer.seek({ timestamp: 0 });
+        audioDecoder = beamcoder.decoder({demuxer, stream_index: audioStreamIndex});
         return;
       }
-      if (packet.stream_index === 0) {
+      if (packet.stream_index === videoStreamIndex) {
         // The video stream. TODO: get this stream index better
         let dec_result = await videoDecoder.decode(packet);
-        console.log(`I just received ${dec_result.frames.length} videoFrames!`);
-        videoFrames = dec_result.frames;
-        const decoded_frame = videoFrames.shift();
+        // TODO: handle multiple frame case
+        const decoded_frame = dec_result.frames[0];
         if (decoded_frame === undefined) {
-          // console.log(`createArtificialMediaStream: no decoded frame after just populating, huh??`);
-          // throw new Error("die");
           return;
         }
         // TODO(jack): find a way to set these more smartly
@@ -54,38 +63,40 @@ function createArtificialMediaStream(filename) {
         videoEncoder.width = decoded_frame.width;
         videoEncoder.height = decoded_frame.height;
         const enc_result = await videoEncoder.encode(decoded_frame);
-        console.log(`I just encoded ${enc_result.packets.length} packets from a videoFrame!`);
+        // TODO: handle multiple packet case
         const enc_packet = enc_result.packets[0];
         const frame = {
           width: videoEncoder.width,
           height: videoEncoder.height,
-          // Need to trim off some of the extra bytes, for some reason
+          // Need to trim off some of the extra bytes, for some reason. TODO investigate
           data: (new Uint8Array(enc_packet.data)).slice(0, 1.5 * videoEncoder.width * videoEncoder.height),
         };
         videoSource.onFrame(frame);
       }
-      if (packet.stream_index === 1) {
+      if (packet.stream_index === audioStreamIndex) {
         // The audio stream. TODO: get this stream index better.
         let dec_result = await audioDecoder.decode(packet);
-        console.log(`I just received ${dec_result.frames.length} audioFrames!`);
-        audioFrames = dec_result.frames;
-        const decoded_frame = audioFrames.shift();
+        // TODO: handle multiple frame case
+        const decoded_frame = dec_result.frames[0];
         if (decoded_frame === undefined) {
           return;
         }
-        audioEncoder.time_base = [1, audioDecoder.sample_rate];
-        const enc_result = await audioEncoder.encode(decoded_frame);
-        console.log(`I just encoded ${enc_result.packets.length} packets from an audioFrame!`);
-        const enc_packet = enc_result.packets[0];
-        // TODO(jack): why does it want this size? this is probably wrong since
-        // there is no audio coming out
-        const samples = new Int16Array(960);
-        samples.set(enc_packet.data);
-        const data = {
-          samples: samples.slice(0, 960 / 2),
-          sampleRate: audioDecoder.sample_rate,
-        };
-        audioSource.onData(data);
+        // NOTE(jack): I couldn't get a beamcoder.encoder to work here, so doing the conversion
+        // from a raw sample myself. I think this code should be pretty efficient after JIT
+        const floatSamples = new Float32Array(decoded_frame.data[0].buffer);
+        const sampleRate = decoded_frame.sample_rate;
+        const maxValue = Math.pow(2, 15) - 1;
+        for (let i = 0; i < floatSamples.length; ++i) {
+          intSamples[i] = floatSamples[i] * maxValue;
+        }
+        for (let sampleOffset = 0; sampleOffset < intSamples.length; sampleOffset += numberOfFrames) {
+          let data = {
+            samples: intSamples.slice(sampleOffset, sampleOffset + numberOfFrames),
+            sampleRate,
+            numberOfFrames,
+          };
+          audioSource.onData(data);
+        }
       }
     }
 
@@ -96,7 +107,7 @@ function createArtificialMediaStream(filename) {
     // 3) create an audio and video publisher interval to publish the results from the
     //    queue at the intervals we read from the file.
     // TODO(jack): find some way to clear this interval later
-    setIntervalAsync(nextFrames, 5000 / 25);
+    setIntervalAsync(nextFrames, 5);
   });
 
   return [videoTrack, audioTrack];
@@ -113,11 +124,11 @@ function beforeOffer(peerConnection) {
   // NOTE(mroberts): This is a hack so that we can get a callback when the
   // RTCPeerConnection is closed. In the future, we can subscribe to
   // "connectionstatechange" events.
-  const { close } = peerConnection;
-  peerConnection.close = function() {
+  const {close} = peerConnection;
+  peerConnection.close = function () {
     tracks.map(track => track.stop());
     return close.apply(this, arguments);
   };
 }
 
-module.exports = { beforeOffer };
+module.exports = {beforeOffer};
