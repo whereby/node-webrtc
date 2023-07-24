60df1aad88b5ead75322a8715bb681e6c1c4d3b4
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2023-07-24 13:12:03 -0400

    0.6.0 windows prebuilt

--- scripts/make-prebuilt.js
+++ scripts/make-prebuilt.js
@@ -5,6 +5,8 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const os = require('os');
+const platform = os.platform();
+const arch = os.arch();
 
 // Run a fresh build
 child_process.execSync('npm run build', { stdio: 'inherit' });
@@ -12,11 +14,24 @@ child_process.execSync('npm run build', { stdio: 'inherit' });
 // Copy the resulting binary to the output folder
 let input_folder = 'build';
 // Windows whyyyy
-if (os.platform() !== 'win32') {
+if (platform !== 'win32') {
   input_folder = `${input_folder}/Release`;
 }
-const output_folder = `prebuilds/${os.platform()}-${os.arch()}`;
+const output_folder = `prebuilds/${platform}-${arch}`;
 fs.copyFileSync(
   `${input_folder}/wrtc.node`,
   `${output_folder}/wrtc.node`,
 );
+
+// Copy version from main package.json to sub package.json
+const main_package_filename = 'package.json';
+const sub_package_filename = `${output_folder}/package.json`;
+const main_package = require(`../${main_package_filename}`);
+const sub_package = require(`../${sub_package_filename}`);
+
+const version = main_package.version;
+main_package.optionalDependencies[`@roamhq/wrtc-${platform}-${arch}`] = version;
+sub_package.version = version;
+
+fs.writeFileSync(main_package_filename, JSON.stringify(main_package, null, 2));
+fs.writeFileSync(sub_package_filename, JSON.stringify(sub_package, null, 2));
