c5e02eadf13a2cf2bd0bfd4421ff94eabe4d01e4
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-02-28 15:04:59 -0500

    update scripts

--- scripts/build-from-source.js
+++ scripts/build-from-source.js
@@ -1,52 +1,48 @@
 #!/usr/bin/env node
 /* eslint no-console:0, no-process-env:0 */
-'use strict';
+"use strict";
+"use strict";
 
-const { spawnSync } = require('child_process');
-const os = require('os');
-const platform = os.platform();
-const arch = process.env.TARGET_ARCH ?? os.arch();
+const os = require("os");
+const { spawnSync } = require("child_process");
+const { platform, arch, buildFolder } = require("./build-vars.js");
 
-const args = ['-O', `build-${platform}-${arch}`, '-a', arch];
+const args = ["-O", buildFolder, "-a", arch];
 
 if (process.env.DEBUG) {
-  args.push(...[
-    '--debug',
-    '--CDCMAKE_EXPORT_COMPILE_COMMANDS=1',
-  ]);
+  args.push(...["--debug", "--CDCMAKE_EXPORT_COMPILE_COMMANDS=1"]);
 }
 
-if (process.platform === 'win32') {
-  args.push(...[
-    '-G',
-    'Ninja',
-  ]);
+if (platform === "win32") {
+  args.push(...["-G", "Ninja"]);
 }
 
-if (process.env.TARGET_ARCH) {
-  args.push(`--CDCMAKE_TOOLCHAIN_FILE=toolchains/${process.platform}-${process.env.TARGET_ARCH}.toolchain`);
+if (arch !== os.arch) {
+  args.push(
+    `--CDCMAKE_TOOLCHAIN_FILE=toolchains/${platform}-${arch}.toolchain`
+  );
 }
 
 function main() {
-  console.log('Running cmake-js ' + args.join(' '));
-  let { status } = spawnSync('cmake-js', ['configure', ...args], {
+  console.log("Running cmake-js " + args.join(" "));
+  let { status } = spawnSync("cmake-js", ["configure", ...args], {
     shell: true,
-    stdio: 'inherit'
+    stdio: "inherit",
   });
   if (status) {
-    throw new Error('cmake-js configure failed for wrtc');
+    throw new Error("cmake-js configure failed for wrtc");
   }
 
-  console.log('Running cmake-js build');
-  status = spawnSync('cmake-js', ['build', ...args], {
+  console.log("Running cmake-js build");
+  status = spawnSync("cmake-js", ["build", ...args], {
     shell: true,
-    stdio: 'inherit'
+    stdio: "inherit",
   }).status;
   if (status) {
-    throw new Error('cmake-js build failed for wrtc');
+    throw new Error("cmake-js build failed for wrtc");
   }
 
-  console.log('Built wrtc');
+  console.log("Built wrtc");
 }
 
 module.exports = main;
--- scripts/make-prebuilt.js
+++ scripts/make-prebuilt.js
@@ -1,30 +1,21 @@
-'use strict';
+"use strict";
 
-// TODO(jack): error handling, cross-platform builds
+// TODO(jack): error handling
 
-const child_process = require('child_process');
-const fs = require('fs');
-const os = require('os');
-const platform = os.platform();
-const arch = os.arch();
+const child_process = require("child_process");
+const fs = require("fs");
+const { platform, arch, buildFolder } = require("./build-vars.js");
 
 // Run a fresh build
-child_process.execSync('npm run build', { stdio: 'inherit' });
+child_process.execSync("npm run build", { stdio: "inherit" });
 
 // Copy the resulting binary to the output folder
-let input_folder = 'build';
-// Windows whyyyy
-if (platform !== 'win32') {
-  input_folder = `${input_folder}/Release`;
-}
+const input_folder = `${buildFolder}/Release`;
 const output_folder = `prebuilds/${platform}-${arch}`;
-fs.copyFileSync(
-  `${input_folder}/wrtc.node`,
-  `${output_folder}/wrtc.node`,
-);
+fs.copyFileSync(`${input_folder}/wrtc.node`, `${output_folder}/wrtc.node`);
 
 // Copy version from main package.json to sub package.json
-const main_package_filename = 'package.json';
+const main_package_filename = "package.json";
 const sub_package_filename = `${output_folder}/package.json`;
 const main_package = require(`../${main_package_filename}`);
 const sub_package = require(`../${sub_package_filename}`);
