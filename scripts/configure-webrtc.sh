f651e4379fa368d66c4b542edd448a30073056bd
Theo Ouzhinski <32401143+theo-o@users.noreply.github.com>
2024-04-29 14:45:38 -0400

    Bump cmake patches and fix scripts

--- scripts/configure-webrtc.sh
+++ scripts/configure-webrtc.sh
@@ -10,6 +10,6 @@ cd ${SOURCE_DIR}
 
 # NOTE(mroberts): Running hooks generates this file, but running hooks also
 # takes too long in CI; so do this manually.
-(cd build/util && python lastchange.py -o LASTCHANGE)
+(cd build/util && python3 lastchange.py -o LASTCHANGE)
 
 gn gen ${BINARY_DIR} "--args=${GN_GEN_ARGS}"
